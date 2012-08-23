4151f2c60d6c01ae29bcf786bb062b926d6fa7c4
coolgo <winerdaxian@163.com>
2012-08-23 11:49:53 +0800

    排除了postList短信信息的发送

--- app/models/RichPost.java
+++ app/models/RichPost.java
@@ -39,8 +39,9 @@ public class RichPost extends Model {
 
 	public static List<RichPost> fetchRichPostsForPaging(Long memberId,
 			Date lastUpdateDate, Integer psize) {
-		String hql = "select post  from RichPost post where createTime<? order by createTime desc ";
-		return RichPost.find(hql, lastUpdateDate).fetch(psize);
+		String hql = "select post  from RichPost post where createTime<? and post.postType<>? order by createTime desc ";
+		return RichPost.find(hql, lastUpdateDate, PostType.PostSMS)
+				.fetch(psize);
 	}
 
 	public static void createRichPost(Member creater, String fileUrl,
